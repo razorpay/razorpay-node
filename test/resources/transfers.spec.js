732c6600ca23ec76934bf7483f921da2c0750c73
Amit Mahbubani <amit.mahbubani@razorpay.com>
2017-05-02 10:43:56 +0530

    include test spec for transfers

---
+++ test/resources/transfers.spec.js
@@ -0,0 +1,57 @@
+'use strict'
+
+const chai = require('chai')
+const { assert } = chai
+const Razorpay = require('../../dist/razorpay')
+const mocker = require('../mocker')
+const equal = require('deep-equal')
+const { getDateInSecs } = require('../../dist/utils/razorpay-utils')
+
+let rzpInstance = new Razorpay({
+  key_id: 'XXX',
+  key_secret: 'YYY'
+})
+
+describe('TRANSFERS', () => {
+  it('Create Direct Transfer', (done) => {
+    let params = {
+      account: 'acc_7jO4N6LScw5CEG',
+      amount: 100,
+      currency: 'INR',
+      notes: {
+        note1: 'This is note1',
+        note2: 'This is note2'
+      }
+    }
+
+    let expectedParams = {
+      recepient: 'acc_7jO4N6LScw5CEG',
+      amount: 100,
+      currency: 'INR',
+      'notes[note1]': 'This is note1',
+      'notes[note2]': 'This is note2'
+    }
+
+    mocker.mock({
+      url: '/transfers',
+      method: 'POST'
+    })
+
+    rzpInstance.transfers.create(params).then((response) => {
+      assert.equal(
+        response.__JUST_FOR_TESTS__.url,
+        '/v1/transfers',
+        'Create Trasnfer request url formed'
+      )
+
+      assert.ok(
+        equal(
+          response.__JUST_FOR_TESTS__.requestBody,
+          expectedParams
+        ),
+        'All params are passed in request body'
+      )
+      done()
+    })
+  })
+})
