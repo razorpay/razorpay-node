806845a8e6dabe31c79784d7d2f4b78367b99884
rzpamidi <prashanth.pamidi@razorpay.com>
2018-06-14 14:18:11 +0530

    Allowing all parameters given to be passed to orders.create

--- dist/resources/orders.js
+++ dist/resources/orders.js
@@ -2,6 +2,8 @@
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+
 var _require = require('../utils/razorpay-utils'),
     normalizeDate = _require.normalizeDate,
     normalizeBoolean = _require.normalizeBoolean,
@@ -56,6 +58,7 @@ module.exports = function (api) {
     create: function create() {
       var params = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
       var callback = arguments[1];
+
       var amount = params.amount,
           currency = params.currency,
           receipt = params.receipt,
@@ -63,7 +66,8 @@ module.exports = function (api) {
           notes = params.notes,
           bank = params.bank,
           method = params.method,
-          account_number = params.account_number;
+          account_number = params.account_number,
+          otherParams = _objectWithoutProperties(params, ['amount', 'currency', 'receipt', 'payment_capture', 'notes', 'bank', 'method', 'account_number']);
 
       currency = currency || 'INR';
 
@@ -80,7 +84,7 @@ module.exports = function (api) {
         currency: currency,
         receipt: receipt,
         payment_capture: normalizeBoolean(payment_capture)
-      }, !!bank && { bank: bank }, !!method && { method: method }, !!account_number && { account_number: account_number }), normalizeNotes(notes));
+      }, !!bank && { bank: bank }, !!method && { method: method }, !!account_number && { account_number: account_number }, otherParams), normalizeNotes(notes));
 
       return api.post({
         url: '/orders',
