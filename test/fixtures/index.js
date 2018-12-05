a5e4aeed7e09fd0687ed7b58e1b1d5befe588ed5
Shreyansh Pandey <s@isomr.co>
2018-12-05 16:46:41 +0530

    added fixtures for the mocker customer data

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -1,9 +1,30 @@
+const commonCustomerParameters = {
+  name: 'test',
+  email: 'test@razorpay.com',
+  contact: '123456789'
+};
+
 module.exports = {
   error: {
     error: {
       code: "BAD_REQUEST_ERROR",
       description: "The count may not be greater than 100.",
       field: "count"
+    }
+  },
+  customer: {
+    commonParameters: commonCustomerParameters,
+    parameters: {
+      ...commonCustomerParameters,
+      notes: {
+        note1: 'This is note1',
+        note2: 'This is note2'
+      }
+    },
+    expectedParameters: {
+      ...commonCustomerParameters,
+      'notes[note1]': 'This is note1',
+      'notes[note2]': 'This is note2'
     }
   }
 };
