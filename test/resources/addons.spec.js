03af790249b73aa0b2da948b0a6456cb18011037
Shreyansh Pandey <s@isomr.co>
2018-12-06 01:41:13 +0530

    included checkForID; renamed runCommonTests to commonTests

--- test/resources/addons.spec.js
+++ test/resources/addons.spec.js
@@ -2,33 +2,13 @@ const { assert } = require('chai');
 const Razorpay = require('../razorpay');
 const mocker = require('../mocker');
 
-const { runCommonTests }  = require('../predefined-tests.js');
+const { commonTests }  = require('../predefined-tests.js');
+const { checkForID } = require('../common');
 
 const SUB_PATH = '/addons';
 const FULL_PATH = `/v1${SUB_PATH}`;
 const TEST_ADDON_ID = 'addon_sometestid';
 const apiObj = Razorpay.addons;
-
-const checkForID = params => {
-
-  let { apiObj, methodName, methodArgs, mockerParams } = params;
-
-  mocker.mock(mockerParams);
-
-  it (`throws an error when no ID parameter is provided (${methodName})`, async () => {
-
-    try {
-      await apiObj[methodName](...methodArgs);
-      throw new Error(
-        `API method '${methodName}' did not throw an error on undefined variable value`
-      );
-    } catch (error) {
-      assert.ok(error);
-    }
-
-  });
-
-}
 
 describe('#Addons', () => {
 
@@ -46,7 +26,7 @@ describe('#Addons', () => {
       }
     });
 
-    runCommonTests({
+    commonTests({
       apiObj,
       methodName,
       methodArgs: [TEST_ADDON_ID],
@@ -73,7 +53,7 @@ describe('#Addons', () => {
       }
     });
 
-    runCommonTests({
+    commonTests({
       apiObj,
       methodName,
       methodArgs: [TEST_ADDON_ID],
