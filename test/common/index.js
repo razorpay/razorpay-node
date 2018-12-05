e9bb496c6e8d3ebf36ca9dc4c9fd214eb0a8d913
Shreyansh Pandey <s@isomr.co>
2018-12-06 01:38:53 +0530

    added checkForID

--- test/common/index.js
+++ test/common/index.js
@@ -1,4 +1,8 @@
+const { assert } = require('chai');
+const mocker = require('../mocker');
+
 module.exports = {
+
   getTestError(summary, expectedVal, gotVal) {
 
     const prettify = val => {
@@ -11,5 +15,27 @@ module.exports = {
       `Got(${typeof gotVal})\n${prettify(gotVal)}`
     );
 
+  },
+
+  checkForID(params) {
+
+    const { apiObj, methodName, methodArgs, mockerParams } = params;
+
+    mocker.mock(mockerParams);
+
+    it (`throws an error when no ID parameter is provided (${methodName})`, async () => {
+
+      try {
+        await apiObj[methodName](...methodArgs);
+        throw new Error(
+          `API method '${methodName}' did not throw an error on undefined variable value`
+        );
+      } catch (error) {
+        assert.ok(error);
+      }
+
+    });
+
   }
+
 };
