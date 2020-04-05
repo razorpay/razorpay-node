ec55aab96a0c033bf6052404a3cd67bb231292bd
Umang Galaiya <umang.galaiya@razorpay.com>
2020-04-06 03:54:42 +0530

    Add types to customer.ts

--- lib/resources/customers.ts
+++ lib/resources/customers.ts
@@ -1,10 +1,11 @@
 'use strict'
 
 import { normalizeNotes } from '../utils/razorpay-utils';
+import { ObjectWithNotes } from '../types';
 
 export default function (api) {
   return {
-    create(params, callback) {
+    create(params: ObjectWithNotes, callback) {
       let { notes, ...rest } = params
       let data = Object.assign(rest, normalizeNotes(notes))
 
@@ -14,7 +15,7 @@ export default function (api) {
       }, callback)
     },
 
-    edit(customerId, params, callback) {
+    edit(customerId: string, params: ObjectWithNotes, callback) {
       let { notes, ...rest } = params
       let data = Object.assign(rest, normalizeNotes(notes))
 
@@ -24,25 +25,25 @@ export default function (api) {
       }, callback)
     },
 
-    fetch(customerId, callback) {
+    fetch(customerId: string, callback) {
       return api.get({
         url: `/customers/${customerId}`
       }, callback)
     },
 
-    fetchTokens(customerId, callback) {
+    fetchTokens(customerId: string, callback) {
       return api.get({
         url: `/customers/${customerId}/tokens`,
       }, callback)
     },
 
-    fetchToken(customerId, tokenId, callback) {
+    fetchToken(customerId: string, tokenId: string, callback) {
       return api.get({
         url: `/customers/${customerId}/tokens/${tokenId}`,
       }, callback)
     },
 
-    deleteToken(customerId, tokenId, callback) {
+    deleteToken(customerId: string, tokenId: string, callback) {
       return api.delete({
         url: `/customers/${customerId}/tokens/${tokenId}`
       }, callback)
