3b9ab9603a53ff031a10262f534f45bf79ed3455
ankitdas13 <ankit.das@razorpay.com>
2021-10-12 13:15:56 +0530

    revert addon

--- dist/resources/addons.js
+++ dist/resources/addons.js
@@ -4,12 +4,7 @@
  * DOCS: https://razorpay.com/docs/subscriptions/api/
  */
 
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
 var Promise = require("promise");
-
-var _require = require('../utils/razorpay-utils'),
-    normalizeDate = _require.normalizeDate;
 
 module.exports = function (api) {
 
@@ -36,48 +31,6 @@ module.exports = function (api) {
 
       return api.get({
         url: url
-      }, callback);
-    },
-    all: function all() {
-      var params = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
-      var callback = arguments[1];
-
-      
-      /*
-       * Get all Addons
-       *
-       * @param {Object} params
-       * @param {Funtion} callback
-       *
-       * @return {Promise}
-       */
-
-      var from = params.from,
-          to = params.to,
-          count = params.count,
-          skip = params.skip,
-          url = BASE_URL;
-
-
-      if (from) {
-        from = normalizeDate(from);
-      }
-
-      if (to) {
-        to = normalizeDate(to);
-      }
-
-      count = Number(count) || 10;
-      skip = Number(skip) || 0;
-
-      return api.get({
-        url: url,
-        data: _extends({}, params, {
-          from: from,
-          to: to,
-          count: count,
-          skip: skip
-        })
       }, callback);
     },
     delete: function _delete(addonId, callback) {
