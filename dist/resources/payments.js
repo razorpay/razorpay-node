9cc8d4a2dba9788775b04c160660066db8b52a03
ankitdas13 <ankit.das@razorpay.com>
2021-10-29 14:36:11 +0530

    payment module updated

--- dist/resources/payments.js
+++ dist/resources/payments.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
 
 var Promise = require("promise");
@@ -7,7 +9,9 @@ var Promise = require("promise");
 var _require = require('../utils/razorpay-utils'),
     normalizeDate = _require.normalizeDate,
     normalizeBoolean = _require.normalizeBoolean,
-    normalizeNotes = _require.normalizeNotes;
+    normalizeNotes = _require.normalizeNotes,
+    isDefined = _require.isDefined,
+    validateWebhookSignature = _require.validateWebhookSignature;
 
 var ID_REQUIRED_MSG = '`payment_id` is mandatory';
 
@@ -83,6 +87,44 @@ module.exports = function (api) {
         data: payload
       }, callback);
     },
+    createPaymentJson: function createPaymentJson(params, callback) {
+      var url = 'payments/create/json',
+          rest = _objectWithoutProperties(params, []),
+          data = Object.assign(rest);
+
+      return api.post({
+        url: url,
+        data: data
+      }, callback);
+    },
+    createRecurringPayment: function createRecurringPayment(params, callback) {
+      var notes = params.notes,
+          rest = _objectWithoutProperties(params, ['notes']);
+
+      var data = Object.assign(rest, normalizeNotes(notes));
+
+      return api.post({
+        url: '/payments/create/recurring',
+        data: data
+      }, callback);
+    },
+    edit: function edit(paymentId) {
+      var params = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+      var callback = arguments[2];
+      var notes = params.notes;
+
+
+      if (!paymentId) {
+        throw new Error('`payment_id` is mandatory');
+      }
+
+      var data = Object.assign(normalizeNotes(notes));
+
+      return api.patch({
+        url: '/payments/' + paymentId,
+        data: data
+      }, callback);
+    },
     refund: function refund(paymentId) {
       var params = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
       var callback = arguments[2];
@@ -98,6 +140,71 @@ module.exports = function (api) {
       return api.post({
         url: '/payments/' + paymentId + '/refund',
         data: data
+      }, callback);
+    },
+    fetchMultipleRefund: function fetchMultipleRefund(paymentId) {
+      var params = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+      var callback = arguments[2];
+
+
+      /*
+       * Fetch multiple refunds for a payment
+       *
+       * @param {String} paymentId 
+       * @param {Object} params
+       * @param {Function} callback
+       *
+       * @return {Promise}
+       */
+
+      var from = params.from,
+          to = params.to,
+          count = params.count,
+          skip = params.skip,
+          url = '/payments/' + paymentId + '/refunds';
+
+
+      return api.get({
+        url: url,
+        data: _extends({}, params, {
+          from: from,
+          to: to,
+          count: count,
+          skip: skip
+        })
+      }, callback);
+    },
+    fetchRefund: function fetchRefund(paymentId, refundId, callback) {
+
+      if (!paymentId) {
+        throw new Error('payment Id` is mandatory');
+      }
+
+      if (!refundId) {
+        throw new Error('refund Id` is mandatory');
+      }
+
+      return api.get({
+        url: '/payments/' + paymentId + '/refunds/' + refundId
+      }, callback);
+    },
+    fetchTransfer: function fetchTransfer(paymentId, callback) {
+
+      /*
+       * Fetch transfers for a payment
+       *
+       * @param {String} paymentId
+       * @param {Function} callback
+       *
+       * @return {Promise}
+       */
+
+      if (!paymentId) {
+        throw new Error('payment Id` is mandatory');
+      }
+
+      return api.get({
+        url: '/payments/' + paymentId + '/transfers'
       }, callback);
     },
     transfer: function transfer(paymentId) {
@@ -134,6 +241,104 @@ module.exports = function (api) {
       return api.get({
         url: '/payments/' + paymentId + '/bank_transfer'
       }, callback);
+    },
+    fetchCardDetails: function fetchCardDetails(paymentId, callback) {
+
+      if (!paymentId) {
+
+        return Promise.reject(ID_REQUIRED_MSG);
+      }
+
+      return api.get({
+        url: '/payments/' + paymentId + '/card'
+      }, callback);
+    },
+    fetchPaymentDowntime: function fetchPaymentDowntime(callback) {
+
+      return api.get({
+        url: '/payments/downtimes'
+      }, callback);
+    },
+    fetchPaymentDowntimeById: function fetchPaymentDowntimeById(downtimeId, callback) {
+
+      /*
+       * Fetch Payment Downtime
+       *
+       * @param {String} downtimeId
+       * @param {Function} callback
+       *
+       * @return {Promise}
+       */
+
+      if (!downtimeId) {
+
+        return Promise.reject("Downtime Id is mandatory");
+      }
+
+      return api.get({
+        url: '/payments/downtimes/' + downtimeId
+      }, callback);
+    },
+    otpSubmit: function otpSubmit(paymentId) {
+      var params = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+      var callback = arguments[2];
+
+
+      /*
+       * OTP Submit
+       *
+       * @param {String} paymentId
+       * @param {Object} params
+       * @param {Function} callback
+       *
+       * @return {Promise}
+       */
+
+      if (!paymentId) {
+
+        return Promise.reject("payment Id is mandatory");
+      }
+
+      return api.post({
+        url: '/payments/' + paymentId + '/otp/submit',
+        data: params
+      }, callback);
+    },
+
+    paymentVerification: function paymentVerification() {
+      var params = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+      var secret = arguments[1];
+
+
+      /*
+       * Payment verfication
+       *
+       * @param {String} secret
+       * @param {Object} params
+       *
+       * @return {Boolean}
+       */
+
+      var signature = params.signature,
+          paymentId = params.payment_id;
+
+      if (!secret) {
+
+        throw new Error("secret is mandatory");
+      }
+
+      if (isDefined(params.order_id) === true) {
+
+        var orderId = params.order_id;
+        var payload = orderId + '|' + paymentId;
+      } else if (isDefined(params.subscription_id) === true) {
+
+        var subscriptionId = params.subscription_id;
+        var payload = paymentId + '|' + subscriptionId;
+      } else {
+        throw new Error('Either order_id or subscription_id is mandatory');
+      }
+      return validateWebhookSignature(payload, signature, secret);
     }
   };
 };
--- test/resources/payments.spec.js
+++ test/resources/payments.spec.js
@@ -7,9 +7,12 @@ const mocker = require('../mocker')
 const equal = require('deep-equal')
 const { getDateInSecs } = require('../../dist/utils/razorpay-utils')
 
-const { runCommonTests }  = require("../../dist/utils/predefined-tests.js");
+const { runCommonTests, runParamsCheckTest, runCallbackCheckTest }  = require("../../dist/utils/predefined-tests.js");
 
-const TEST_PAYMENT_ID = 'pay_sometestId';
+const TEST_PAYMENT_ID = 'pay_sometestId',
+      TEST_REFUND_ID = 'rfnd_sometestId',
+      TEST_DOWNTIME_ID = 'down_sometestId',
+      apiObj = rzpInstance.payments;
 
 describe('PAYMENTS', () => {
   describe('Fetch Payments', () => {
@@ -90,6 +93,60 @@ describe('PAYMENTS', () => {
           response.__JUST_FOR_TESTS__.url,
           `/v1/payments/${paymentId}`,
           'Fetch payment url formed correctly'
+        )
+        done()
+      })
+    })
+  })
+
+  describe('Fetch Transfer', () => {
+    it('Throw error when paymentId is not provided', () => {
+      assert.throws(
+        rzpInstance.payments.fetch,
+        '`payment_id` is mandatory',
+        'Should throw exception when paymentId is not provided'
+      )
+    })
+
+    it('Forms the payment fetch request', (done) => {
+      let paymentId = 'pay_sometestId'
+
+      mocker.mock({
+        url: `/payments/${paymentId}/transfers`
+      })
+
+      rzpInstance.payments.fetchTransfer(paymentId).then((response) => {
+        assert.equal(
+          response.__JUST_FOR_TESTS__.url,
+          `/v1/payments/${paymentId}/transfers`,
+          'Fetch payment url formed correctly'
+        )
+        done()
+      })
+    })
+  })
+
+  describe('edit payment', () => {
+    it('edit payment', (done) => {
+      let paymentId = 'pay_sometestId'
+
+      let params = {
+        notes: {
+          note1: 'This is note1',
+          note2: 'This is note2'
+        }
+      }
+
+      mocker.mock({
+        url: `/payments/${paymentId}`,
+        method : 'PATCH'
+      })
+
+      rzpInstance.payments.edit(paymentId, params).then((response) => {
+        assert.equal(
+          response.__JUST_FOR_TESTS__.url,
+          `/v1/payments/${paymentId}`,
+          'Request url formed correctly'
         )
         done()
       })
@@ -269,5 +326,232 @@ describe('PAYMENTS', () => {
       },
       expectedUrl: `/v1/payments/${TEST_PAYMENT_ID}/bank_transfer`
     });
+  })
+
+  describe('Fetch card details with paymentId', () => {
+  
+    it('Reject the promise if payment id is not provided', (done) => {
+
+      rzpInstance.payments.fetchCardDetails()
+                 .then(() => {
+
+                   done(new Error(`method fetchCardDetails does not check` +
+                                  ` for payment_id`))
+                 }, () => {
+
+                   done();
+                 });
+    });
+
+    runCommonTests({
+      apiObj: rzpInstance.payments,
+      methodName: "fetchCardDetails",
+      methodArgs: [TEST_PAYMENT_ID],
+      mockerParams: {
+        url: `/payments/${TEST_PAYMENT_ID}/card`
+      },
+      expectedUrl: `/v1/payments/${TEST_PAYMENT_ID}/card`
+    });
+  })
+
+  describe('Payment Downtime ', () => {
+    it('Fetch Payment Downtime Details', (done) => {
+      mocker.mock({
+        url: '/payments/downtimes'
+      })
+
+      rzpInstance.payments.fetchPaymentDowntime().then((response) => {
+        assert.ok(equal(
+          response.__JUST_FOR_TESTS__.url,
+          '/v1/payments/downtimes'
+        ))
+        done()
+      })
+    })
+  })
+  
+  describe('Fetch Payment Downtime', () => {
+  
+    it('Reject the promise if downtime id is not provided', (done) => {
+
+      rzpInstance.payments.fetchPaymentDowntimeById()
+      .then(() => {
+
+        done(new Error(`method fetchCardDetails does not check` +
+                      ` for payment_id`))
+      }, () => {
+         done();
+      });
+    });
+
+    runCommonTests({
+      apiObj: rzpInstance.payments,
+      methodName: "fetchPaymentDowntimeById",
+      methodArgs: [TEST_DOWNTIME_ID],
+      mockerParams: {
+        url: `/payments/downtimes/${TEST_DOWNTIME_ID}`
+      },
+      expectedUrl: `/v1/payments/downtimes/${TEST_DOWNTIME_ID}`
+    });
+  })
+
+  describe("Fetch multiple refunds for a payment", () => {
+
+    let methodName = "fetchMultipleRefund",
+        params = {
+          "skip": 0,
+          "count": 10
+        },
+        methodArgs = [TEST_PAYMENT_ID, params],
+        expectedParams = {
+          ...params
+        },
+        mockerParams = {
+          url: '/payments/' + TEST_PAYMENT_ID + '/refunds'
+        };
+
+      runParamsCheckTest({
+        apiObj,
+        methodName,
+        methodArgs,
+        mockerParams,
+        expectedParams,
+        testTitle: "Check if all params passed are being sent"
+      });
+
+      methodArgs = [TEST_PAYMENT_ID ,{}];
+
+      runCallbackCheckTest({
+        apiObj,
+        methodName,
+        mockerParams,
+        methodArgs
+      });
+  });
+
+  describe('Fetch a Specific Refund for a Payment', () => {
+    it('Fetch Refund', (done) => {
+
+      mocker.mock({
+        url: '/payments/'+ TEST_PAYMENT_ID + '/refunds/' + TEST_REFUND_ID
+      })
+
+      rzpInstance.payments.fetchRefund(TEST_PAYMENT_ID,TEST_REFUND_ID).then((response) => {
+        assert.ok(equal(
+          response.__JUST_FOR_TESTS__.url,
+          '/v1/payments/'+ TEST_PAYMENT_ID + '/refunds/' + TEST_REFUND_ID
+        ))
+        done()
+      })
+    })
+  })
+
+  describe('Server to server json v2', () => {
+    it('Create Payment Json', (done) => {
+      
+      const params = {
+        "amount": "100",
+        "currency": "INR",
+        "email": "gaurav.kumar@example.com",
+        "contact": "999999999",
+        "order_id": "order_sometestId",
+        "method": "card",
+        "card": {
+          "number": "4854980604708430",
+          "cvv": "123",
+          "expiry_month": "12",
+          "expiry_year": "21",
+          "name": "Gaurav Kumar"
+        }
+      }
+
+      mocker.mock({
+        url: '/payments/create/json',
+        method : 'POST'
+      })
+
+      rzpInstance.payments.createPaymentJson(params).then((response) => {
+        assert.ok(equal(
+          response.__JUST_FOR_TESTS__.url,
+          '/v1/payments/create/json'
+        ))
+        done()
+      })
+    })
+  })
+
+  describe('Recurring Payment', () => {
+    it('Create a Recurring Payment', (done) => {
+      
+      const params = {
+        "email": "gaurav.kumar@example.com",
+        "contact": "9123456789",
+        "amount": 1000,
+        "currency": "INR",
+        "order_id": "order_1Aa00000000002",
+        "customer_id": "cust_1Aa00000000001",
+        "token": "token_1Aa00000000001",
+        "recurring": "1",
+        "description": "Creating recurring payment for Gaurav Kumar",
+        "notes": {
+          "note_key 1": "Beam me up Scotty",
+          "note_key 2": "Tea. Earl Gray. Hot."
+        }
+      }
+
+      mocker.mock({
+        url: '/payments/create/recurring',
+        method : 'POST'
+      })
+
+      rzpInstance.payments.createRecurringPayment(params).then((response) => {
+        assert.ok(equal(
+          response.__JUST_FOR_TESTS__.url,
+          '/v1/payments/create/recurring'
+        ))
+        done()
+      })
+    })
+  })
+
+  it('Otp Submit', (done) => {
+    let paymentId = 'pay_sometestId'
+
+    let params = {
+      otp : 1234
+    }
+
+    mocker.mock({
+      url: `/payments/${paymentId}/otp/submit`,
+      method : 'POST'
+    })
+
+    rzpInstance.payments.otpSubmit(paymentId, params).then((response) => {
+      assert.equal(
+        response.__JUST_FOR_TESTS__.url,
+        `/v1/payments/${paymentId}/otp/submit`,
+        'Request url formed correctly'
+      )
+      done()
+    })
+  })
+
+  describe('Payment Verfication', () => {
+    it('Payment Verfication', (done) => {
+      
+      let params = {
+           'subscription_id':'sub_ID6MOhgkcoHj9I',
+           'payment_id':'pay_IDZNwZZFtnjyym',
+           'signature':'601f383334975c714c91a7d97dd723eb56520318355863dcf3821c0d07a17693'
+      }
+
+       let secret = 'EnLs21M47BllR3X8PSFtjtbd';
+
+       const verify = rzpInstance.payments.paymentVerification(params,secret);
+
+        if(verify){
+          done();
+        }
+     })
   })
 })
