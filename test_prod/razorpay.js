89bcc7d7de13054ff6a92ae32d5d20234aa579fc
ankitdas13 <ankit.das@razorpay.com>
2023-07-11 15:19:26 +0530

    add ci.yml

---
+++ test_prod/razorpay.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const Razorpay = require("../dist/razorpay");
+
+module.exports = new Razorpay({
+  key_id: process.env.API_KEY || "",
+  key_secret: process.env.API_SECRET || ""
+});
