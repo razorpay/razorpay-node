62f166e382ddb4d9e04704798bb42ff23c9a4337
Ankit Das <89454448+ankitdas13@users.noreply.github.com>
2023-08-01 15:25:38 +0530

    Update razorpay.js

--- test_prod/razorpay.js
+++ test_prod/razorpay.js
@@ -2,7 +2,23 @@
 
 const Razorpay = require("../dist/razorpay");
 
-module.exports = new Razorpay({
+class RazorpayBeta extends Razorpay {
+  constructor(options) {
+    super(options)
+    this.api.rq = request.defaults({
+      baseUrl: hostUrl,
+      json: true,
+      auth: {
+        user: options.key_id,
+        pass: options.key_secret
+      }
+    })
+  }
+}
+
+
+module.exports = new RazorpayBeta({
   key_id: process.env.API_KEY || "",
-  key_secret: process.env.API_SECRET || ""
+  key_secret: process.env.API_SECRET || "",
+  hostUrl : "https://api-web.dev.razorpay.in"
 });
