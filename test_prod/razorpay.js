5170381dd27b2afd9f09d77da0e0c9bc9e417b9c
ankitdas13 <ankit.das@razorpay.com>
2023-08-01 12:48:27 +0530

    added beta url

---
+++ test_prod/razorpay.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Razorpay = require("../dist/razorpay");
+let request = require('request-promise');
+
+class RazorpayTest extends Razorpay {
+  constructor(options) {
+    super(options)
+    this.api.rq = request.defaults({
+      baseUrl: "https://api-web.dev.razorpay.in",
+      json: true,
+      auth: {
+        user: options.key_id,
+        pass: options.key_secret
+      }
+    })
+  }
+}
+
+
+module.exports = new RazorpayTest({
+  key_id: process.env.API_KEY || "",
+  key_secret: process.env.API_SECRET || ""
+});
