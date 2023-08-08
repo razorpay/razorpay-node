6551083953f99fab6c2a58417693fb043d1dbcfd
ankitdas13 <ankit.das@razorpay.com>
2023-08-08 15:51:18 +0530

    remove e2e & prod.yml

--- e2e/razorpay.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const Razorpay = require("../dist/razorpay");
-
-module.exports = new Razorpay({
-  key_id: process.env.PRODUCT_API_KEY || "",
-  key_secret: process.env.PRODUCT_API_SECRET || ""
-});
